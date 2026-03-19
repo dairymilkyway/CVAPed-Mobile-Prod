7441d3f98ca58d6621123de7b138ae9f1c1d6da1
dairymilkyway <skythekid211@gmail.com>
2026-03-19 13:07:02 +0800

    feat: add project ID to app configuration for Expo and enhance extra properties

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,10 +1,18 @@
 const { expo } = require('./app.json');
 
 const googleServicesFile = process.env.GOOGLE_SERVICES_JSON || './google-services.json';
+const projectId = '01b0ad1a-a7a8-4d70-9b80-65c983dbda20';
 
 module.exports = {
   expo: {
     ...expo,
+    extra: {
+      ...(expo.extra || {}),
+      eas: {
+        ...((expo.extra && expo.extra.eas) || {}),
+        projectId,
+      },
+    },
     android: {
       ...expo.android,
       googleServicesFile,
